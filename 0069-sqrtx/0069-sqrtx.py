b28896c005b46cb2e95585d3eca6c8243a30bb57
Morgan Rockett <morgan.rockett@tufts.edu>
2023-02-23 17:28:26 -0500

    Time: 2760 ms (9.48%), Space: 13.8 MB (94.40%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:    
+
+        my_list = [-1, -1]
+
+        for num in range(2**31):
+            ans = num * num
+            if ans < x:
+                my_list = [num]
+            elif ans == x:
+                return num
+            elif ans > x:
+                return my_list[0]
+
+        return my_list[0]
+    
+#     0110 -> 0010
+#     4 -> 2
+    
+#     8 -> 2.8 -> 2
+#     1000 -> 0010
+
+#     21 -> 4.6 -> 4
+#     10101 -> 0100
+
+#     37 -> 6.1 -> 6
