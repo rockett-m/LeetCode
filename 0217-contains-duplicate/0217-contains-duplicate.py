b87dec34e481be89b96d62f7aa6b98c12afda5fd
Morgan Rockett <morgan.rockett@tufts.edu>
2023-02-23 13:31:57 -0500

    Time: 587 ms (35.53%), Space: 28.8 MB (49.25%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,6 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) > len(list(set(nums))):
+            return True
+        else:
+            return False
