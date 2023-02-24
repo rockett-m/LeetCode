42af69e8c12d58786d87f2f3202d75f5b7b81f0e
Morgan Rockett <morgan.rockett@tufts.edu>
2023-02-24 17:23:37 -0500

    Time: 67 ms (9.84%), Space: 14 MB (21.78%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,6 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        if len(s) == 0: return t
+        # if len(s) == 0: return t
         my_list = list(t)
         for i in range(len(s)):
             if s[i] in my_list:
