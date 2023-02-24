bd58afa8a716ca637fc87c17d8490c6fcccbe713
Morgan Rockett <morgan.rockett@tufts.edu>
2023-02-24 17:04:55 -0500

    Time: 430 ms (16.13%), Space: 14.2 MB (49.65%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -7,8 +7,8 @@ class Solution:
             if letter not in my_dict.keys():
                 my_dict[letter] = [idx, 1]
             else:
-                count =  my_dict[letter][1] + 1
-                my_dict.update({letter:[idx, count]})
+                # count = my_dict[letter][1] + 1
+                my_dict.update({letter:[idx, my_dict[letter][1] + 1]})
 
         for k, v in my_dict.items():
             if v[1] == 1: # only 1 counts are good
