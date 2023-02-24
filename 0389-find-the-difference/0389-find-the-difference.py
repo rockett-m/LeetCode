cecfb158bc61b21dfb075ed2b95f3281429a7b1d
Morgan Rockett <morgan.rockett@tufts.edu>
2023-02-24 17:26:10 -0500

    Time: 26 ms (97.56%), Space: 13.9 MB (63.75%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,8 +1,6 @@
 from collections import Counter
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        # if len(s) == 0: return t
-
         res = Counter(t) - Counter(s)
         for k,v in res.items():
             if v == 1:
