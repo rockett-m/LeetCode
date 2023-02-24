b3e883bbc3f1233da079f37d828c8b5ff09353f0
Morgan Rockett <morgan.rockett@tufts.edu>
2023-02-24 17:25:42 -0500

    Time: 34 ms (76.90%), Space: 13.9 MB (21.78%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,10 +1,16 @@
+from collections import Counter
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        # if len(s) == 0: return t
-        my_list = list(t)
-        for i in range(len(s)):
-            if s[i] in my_list:
-                my_list.pop(my_list.index(s[i]))                
-            else:
-                return s[i]
-        return my_list[0]
+        if len(s) == 0: return t
+        # my_list = list(t)
+        # for i in range(len(s)):
+        #     if s[i] in my_list:
+        #         my_list.pop(my_list.index(s[i]))                
+        #     else:
+        #         return s[i]
+        # return my_list[0]
+        res = Counter(t) - Counter(s)
+        for k,v in res.items():
+            if v == 1:
+                return k
+        return -1
