4cb7efc59c04a0d16d3cf88fe86274292a467b8a
Morgan Rockett <morgan.rockett@tufts.edu>
2023-02-24 14:54:53 -0500

    Time: 41 ms (39.77%), Space: 13.9 MB (51.57%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        while n > 1:
+            new = []
+            nn = str(n)
+            for i in range(len(nn)):
+                new.append(int(nn[i]) ** 2)
+            total = 0
+            for x in new:
+                total += x
+            if total == 1:
+                return True
+            if total < 10 and total % 2 == 0:
+                return False
+            n = total
+        
+        if n == 1:
+            return True
+        return False
