efd737e8dfc0070ad89403277caaeab5ce5b3bfa
Morgan Rockett <morgan.rockett@tufts.edu>
2023-02-24 20:14:16 -0500

    Time: 48 ms (93.56%), Space: 15.5 MB (47.94%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -10,19 +10,3 @@ class Solution:
             return high_to_low[2]
         else:
             return high_to_low[0]
-        
-        
-#         counts = Counter(nums)
-#         print(counts)
-#         print([i for i in counts if counts[i] == 1])
-#         uniq = [i for i in counts if counts[i] == 1]
-#         if len(uniq) >= 3:
-#                     return sorted(list(set(nums)), reverse=True)[2]
-
-#             # return uniq[2]
-#             return sorted(counts, key=counts.get, reverse=True)[2]
-#         else:
-#             # return uniq[0]
-#             # sorted(counts, key=counts.get, reverse=True)[2]
-#             # counts.sorted()[0]
-#             return counts[0]
