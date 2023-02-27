bf26e03b33930b7a2d15468f5b1744804d6a6120
Morgan Rockett <morgan.rockett@tufts.edu>
2023-02-26 20:36:21 -0500

    Time: 28 ms (84.09%), Space: 13.8 MB (47.68%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -2,10 +2,8 @@ class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
         conv = list(str(bin(n)[2:]))
         mid = floor(len(conv)//2)
-        if len(conv) % 2 == 0: # even
-            if conv[0:mid] is conv[mid+1:].reverse():
-                return True
-        else: # odd
-            if conv[0:mid] is conv[mid:].reverse():
-                return True
+        if (len(conv) % 2 == 0) and (conv[0:mid] is conv[mid+1:].reverse()): # even
+            return True
+        elif conv[0:mid] is conv[mid:].reverse(): # share mid
+            return True
         return False
