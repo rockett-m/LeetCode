f33cec918cf683d7ca8438d5b8c4ce43ff3aa408
Morgan Rockett <morgan.rockett@tufts.edu>
2023-05-07 23:27:23 -0400

    Time: 58 ms (5.89%), Space: 16.4 MB (8.92%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        count = Counter(s)
+        if count['A'] >= 2:
+            return False
+
+        late_count = 0
+        prev = ''
+        for idx, let in enumerate(s):
+            print(idx, let)
+            if prev == '':
+                prev = let
+            if let == 'L':
+                late_count += 1
+            elif let != 'L':
+                late_count = 0
+            if late_count >= 3:
+                return False
+            print(late_count)
+            prev = let
+        return True
