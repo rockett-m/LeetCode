54e287ed9790f843c51c32c64db3c4690c615d33
Morgan Rockett <morgan.rockett@tufts.edu>
2023-08-31 07:42:14 -0400

    Time: 170 ms (21.45%), Space: 19.9 MB (5.04%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        my_dict = Counter(s)
+        
+        single = True
+        for k,v in my_dict.items():
+            if v > 1:
+                single = False; break
+        
+        if single:
+            return 1
+        
+        temp = []
+        sub = ''
+        for i in range(len(s)):
+            if i == 0:
+                sub = s[i]
+                continue
+            if s[i] not in sub:
+                sub += s[i]
+            else:
+                temp.append(sub)
+                sub = s[i]
+            if i == len(s) - 1:
+                temp.append(sub)
+        print(temp)
+        return len(temp)
+            
