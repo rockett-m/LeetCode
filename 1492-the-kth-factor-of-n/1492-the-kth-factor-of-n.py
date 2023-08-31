72d343f59f22770c5e5ff6716510c8d81ee533e0
Morgan Rockett <morgan.rockett@tufts.edu>
2023-08-31 07:29:15 -0400

    Time: 43 ms (56.10%), Space: 16.3 MB (30.63%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        my_li = []
+        for i in range(1, n+1):
+            if n % i == 0:
+                my_li.append(i)
+        
+        print(my_li)
+        if len(my_li) < k:
+            return -1
+        
+        return my_li[k-1]
