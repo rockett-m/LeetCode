29b80580ac7af6c2d23de2d8b46ad3894a8f7e26
Morgan Rockett <morgan.rockett@tufts.edu>
2023-09-20 12:13:01 -0400

    Time: 42 ms (34.56%), Space: 16.1 MB (87.96%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
@@ -3,8 +3,8 @@ class Solution:
         out = []
         for idx, val in enumerate(s):
             if idx % 2 != 0: # odd - number expected
-                temp = s[idx-1] + str(chr(ord(s[idx-1])+int(val)))
-                out.append(temp)
+                out.append(s[idx-1])
+                out.append(str(chr(ord(s[idx-1])+int(val))))
             if (idx == len(s) - 1) and (idx % 2 == 0): # even final just append
                 out.append(val)
         return ''.join(out)
