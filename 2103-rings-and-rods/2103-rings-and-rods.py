4dbec003d9919a17759535616722998e04fc046d
Morgan Rockett <morgan.rockett@tufts.edu>
2023-09-19 22:44:56 -0400

    Time: 42 ms (33.68%), Space: 16.3 MB (64.97%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        my_dict = dict()
+        for idx, val in enumerate(rings):
+            if idx % 2 == 0: # color
+                rod = rings[idx+1] # rod
+                if rod not in my_dict.keys():
+                    my_dict[rod] = set(val)
+                else:
+                    my_dict[rod].add(val)
+        count = 0
+        for k,v in my_dict.items():
+            if len(v) == 3:
+                count += 1
+        return count
+        
