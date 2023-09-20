73041e3ad4b06b198241a1bc9c5e9cf87266e67a
Morgan Rockett <morgan.rockett@tufts.edu>
2023-09-19 22:46:25 -0400

    Time: 32 ms (91.71%), Space: 16.1 MB (89.02%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -1,6 +1,7 @@
 class Solution:
     def countPoints(self, rings: str) -> int:
         my_dict = dict()
+        count = 0
         for idx, val in enumerate(rings):
             if idx % 2 == 0: # color
                 rod = rings[idx+1] # rod
@@ -8,7 +9,7 @@ class Solution:
                     my_dict[rod] = set(val)
                 else:
                     my_dict[rod].add(val)
-        count = 0
+
         for k,v in my_dict.items():
             if len(v) == 3:
                 count += 1
