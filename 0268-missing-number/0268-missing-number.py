8b3b4a807653ab60fae9408136ba491388df6bf5
Morgan Rockett <morgan.rockett@tufts.edu>
2023-09-30 20:47:02 -0400

    Time: 124 ms (63.66%), Space: 19.2 MB (7.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        expected = [ x for x in range(0, len(nums)+1)]
-        return list(set(expected) ^ set(nums))[0]
+        # expected = 
+        return list(set([ x for x in range(0, len(nums)+1)]) ^ set(nums))[0]
 
