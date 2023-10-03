f81f9246f2cbbc9f700ef94311eafce0381ed8b4
Morgan Rockett <morgan.rockett@tufts.edu>
2023-10-03 15:42:34 -0400

    Time: 55 ms (65.91%), Space: 16.4 MB (74.23%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -3,10 +3,14 @@ class Solution:
         vert = 0
         side = 0
         
-        for i in moves:
-            if i == 'U': vert += 1
-            elif i == 'D': vert -= 1
-            elif i == 'L': side -= 1
-            elif i == 'R': side += 1
+        ans = Counter(moves)
         
-        return True if vert == 0 and side == 0 else False
+        return True if ans['L'] - ans['R'] == 0 and ans['U'] - ans['D'] == 0 else False
+        
+#         for i in moves:
+#             if i == 'U': vert += 1
+#             elif i == 'D': vert -= 1
+#             elif i == 'L': side -= 1
+#             elif i == 'R': side += 1
+        
+#         return True if vert == 0 and side == 0 else False
