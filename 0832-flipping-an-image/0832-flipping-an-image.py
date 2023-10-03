3dc8bb64b58b522b709fddd8d6015645fd1dc966
Morgan Rockett <morgan.rockett@tufts.edu>
2023-10-03 17:28:07 -0400

    Time: 60 ms (22.62%), Space: 16.3 MB (67.46%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.py
+++ 0832-flipping-an-image/0832-flipping-an-image.py
@@ -2,6 +2,7 @@ class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
         new = []
         for idx, val in enumerate(image):
-            temp = [ 1^x for x in val[::-1] ]
+            temp = val[::-1]
+            temp = [ 1^x for x in temp ]
             new.append(temp)
         return new
