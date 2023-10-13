38086d78041abd5894db48ca42d33ff0fb20ec3c
Morgan Rockett <morgan.rockett@tufts.edu>
2023-10-12 23:33:08 -0400

    Time: 2162 ms (5.02%), Space: 39.2 MB (70.74%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -0,0 +1,20 @@
+class Solution:
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        total = 0
+        for typ in ['M', 'P', 'G']:
+            travel_time = 0
+            found = False
+            for idx, val in enumerate(reversed(garbage)):
+                c = Counter(val)
+                if typ in c.keys():
+                    house_time = c[typ]
+                    total += house_time
+                    if not found:
+                        final_house = len(garbage) - 1 - idx
+                        found = True
+
+            if found:
+                travel_time = sum([z for z in travel[0:final_house]])
+                total += travel_time
+            
+        return total
