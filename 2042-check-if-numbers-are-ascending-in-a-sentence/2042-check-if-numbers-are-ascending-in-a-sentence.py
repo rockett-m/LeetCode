0551f07fdb71577b525abc5c8da70c0109f23b73
Morgan Rockett <morgan.rockett@tufts.edu>
2023-10-13 01:04:08 -0400

    Time: 39 ms (48.53%), Space: 16.2 MB (50.24%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.py
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.py
@@ -2,14 +2,12 @@ class Solution:
     def areNumbersAscending(self, s: str) -> bool:
         nums = []
         for t in s.split(' '):
-            if t.isalpha():
-                continue
-            if len(nums) == 0:
-                nums.append(int(t))
-            else:
-                if nums[-1] < int(t):
+            if t.isnumeric():
+                if len(nums) == 0:
                     nums.append(int(t))
                 else:
-                    return False
-
+                    if nums[-1] < int(t):
+                        nums.append(int(t))
+                    else:
+                        return False
         return True
