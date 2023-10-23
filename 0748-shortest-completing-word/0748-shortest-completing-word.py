2533fdd72c52905f8e348dc53a7758718d7091a1
Morgan Rockett <morgan.rockett@tufts.edu>
2023-10-23 16:33:06 -0400

    Time: 102 ms (22.65%), Space: 16.5 MB (81.63%) - LeetHub

---
+++ 0748-shortest-completing-word/0748-shortest-completing-word.py
@@ -0,0 +1,39 @@
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        lets = licensePlate.strip(' ').lower()
+        let = []
+        for i in lets:
+            if i.isalpha():
+                let.append(i)
+        print(let)
+        
+        req = Counter(let)
+        
+        # loop through
+        # use Counter on word to scan - to solve repeat letters
+        # count >= required letter amount
+        
+        minn = -1
+        out = []
+        for w in words:
+            bad = False
+            wc = Counter(w)
+            for kreq, vreq in req.items():
+                if kreq in wc.keys():
+                    if vreq > wc[kreq]:
+                        bad = True
+                else:
+                    bad = True # bad etc
+
+            if not bad:        
+                if minn < 0 or len(w) < minn:
+                    minn = len(w)
+                    out = [w]
+        
+        good = ''
+        for i in range(len(out)):
+            if len(out[i]) <= minn:
+                good = out[i]
+                break
+        
+        return good
