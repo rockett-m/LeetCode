ad9020d704886ceecae8674707bdba361bca2a26
Morgan Rockett <morgan.rockett@tufts.edu>
2023-11-23 10:03:40 -0500

    Time: 65 ms (14.95%), Space: 16.4 MB (34.62%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.py
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -3,19 +3,15 @@ class Solution:
         my_dict = OrderedDict()
         for cpd in cpdomains:
             num, domain = cpd.split(' ')
-            num = int(num)
             fields = domain.split('.')
             for idx, field in enumerate(fields):
                 
                 key = '.'.join(fields[idx:])
                 if key in my_dict.keys():
-                    my_dict[key] += num
-                
+                    my_dict[key] += int(num)
                 else:
-                    my_dict[key] = num
+                    my_dict[key] = int(num)
 
-
-        print(my_dict.items())
         out = []
         for k,v in my_dict.items():
             item = f'{v} {k}'
