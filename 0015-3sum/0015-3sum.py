b8c6078a6aa4efac982c901b14f484af5347411c
Morgan Rockett <morgan.rockett@tufts.edu>
2023-11-24 10:16:44 -0500

    Time: 8600 ms (5.00%), Space: 20.5 MB (31.51%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,39 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ''' passes 308 / 312 cases and runtime fail
+        
+        output = []
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+
+                    trip = list([nums[i], nums[j], nums[k]])
+                    if sum(trip) == 0: # save compute time
+                        if sorted(trip) not in output:
+                            output.append(sorted(trip))
+        return output
+        '''
+
+        # ''' passes 308 / 312 cases and runtime fail
+        output = []
+        nums = sorted(nums)
+        for i in range(len(nums)-2): # allows 2nd and 3rd digits to not overflow
+            lp = i+1 # 1 greater than fixed idx
+            rp = len(nums) - 1 # last
+            while lp < rp:
+                trip = list([nums[i], nums[lp], nums[rp]])
+                
+                if sum(trip) == 0:
+                    if trip not in output:
+                        output.append(trip)
+
+                # how do you decide which to increment/decrement?
+                # see how the sum compares 
+                
+                if sum(trip) < 0:
+                    lp += 1
+                else:
+                    rp -= 1
+                        
+        return output
+        # '''
