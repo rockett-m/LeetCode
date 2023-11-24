0edc6c3316e5e0f1ef5621e4cf5ce0ffda224ad7
Morgan Rockett <morgan.rockett@tufts.edu>
2023-11-24 10:45:31 -0500

    Time: 7452 ms (5.00%), Space: 20.4 MB (70.42%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,20 +1,6 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ''' passes 308 / 312 cases and runtime fail
-        
-        output = []
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                for k in range(j+1, len(nums)):
 
-                    trip = list([nums[i], nums[j], nums[k]])
-                    if sum(trip) == 0: # save compute time
-                        if sorted(trip) not in output:
-                            output.append(sorted(trip))
-        return output
-        '''
-
-        # ''' passes 308 / 312 cases and runtime fail
         output = []
         nums = sorted(nums)
         for i in range(len(nums)-2): # allows 2nd and 3rd digits to not overflow
@@ -23,18 +9,17 @@ class Solution:
             while lp < rp:
                 trip = list([nums[i], nums[lp], nums[rp]])
                 
+                # how do you decide which to increment/decrement?
+                # see how the sum compares (nums are sorted remember)
                 if sum(trip) == 0:
                     if trip not in output:
                         output.append(trip)
+                    # change both pointers because we need unique triplets
                     lp += 1
                     rp -= 1
-                # how do you decide which to increment/decrement?
-                # see how the sum compares (nums are sorted remember)
-                # can't edit both pointers at once or could skip combos
                 elif sum(trip) < 0:
                     lp += 1
                 else:
                     rp -= 1
                         
         return output
-        # '''
