c22e5d4fafcb55e206dffcc5e022c4323e7dab47
Morgan Rockett <morgan.rockett@tufts.edu>
2023-11-25 14:30:19 -0500

    Time: 102 ms (86.14%), Space: 17.7 MB (5.02%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.py
+++ 0908-smallest-range-i/0908-smallest-range-i.py
@@ -1,6 +1,3 @@
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
-        # for idx, val in enumerate(sorted(nums)):
-        #     nums[idx] = nums[idx] + k, -k
-            
-        return max(0, (max(nums)-k) - (min(nums)+k))
+        return max(0, (max(nums)-2*k) - (min(nums)))
