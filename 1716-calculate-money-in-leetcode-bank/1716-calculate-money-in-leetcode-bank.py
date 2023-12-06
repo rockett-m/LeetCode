b08553a8e632280cc34abbe04fc225eb5b6a764c
Morgan Rockett <morgan.rockett@tufts.edu>
2023-12-06 01:25:29 -0500

    Time: 50 ms (5.70%), Space: 16.3 MB (7.77%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -3,7 +3,6 @@ class Solution:
         base = 0
         total = 0
         for i in range(1, n+1):
-            days_left = n-1
             if (i-1) % 7 == 0:
                 base += 1
             total += base+(i-1)%7
