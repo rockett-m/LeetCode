70da81b2559c3c9dc85b115ca2ddccb2e4b1db79
Morgan Rockett <morgan.rockett@tufts.edu>
2023-12-11 07:55:37 -0500

    Time: 80 ms (95.79%), Space: 18 MB (30.41%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
@@ -1,5 +1,8 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        c = Counter(arr)
-        high = max(c.values())
-        return [k for k,v in c.items() if v == high][0]
+        # c = Counter(arr)
+        # high = max(c.values())
+        # return [k for k,v in c.items() if v == high][0]
+        from statistics import mode
+        return mode(arr)
+    
