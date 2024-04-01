981bb6f10b44d7a072387d2cd3d10ff6ebb1e23f
Morgan Rockett <morgan.rockett@tufts.edu>
2024-04-01 19:11:17 -0400

    Time: 39 ms (36.82%), Space: 16.6 MB (33.75%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -5,29 +5,31 @@ class Solution:
 
         if not nums:
             return 0
-        if len(nums) == 1:
+        elif len(nums) == 1:
             return nums[0]
         
         self.memo_no_first = OrderedDict((idx, max(0, val)) for idx, val in enumerate(nums[1:]))
         self.memo_no_last =  OrderedDict((idx, max(0, val)) for idx, val in enumerate(nums[:-1]))
         
-        return max(self._memo_rob(nums[1:], len(nums)-2, True), self._memo_rob(nums[:-1], len(nums)-2, False))
+        i = len(nums[1:])-1
+
+        return max(self._memo_rob(nums[1:], i, True), self._memo_rob(nums[:-1], i, False))
         
 
     def _memo_rob(self, houses, idx: int, no_first: True):
 
         if no_first:
-            for h, (key, val) in enumerate(self.memo_no_first.items()):
+            for h in range(1, idx+1):
                 if h == 1:
                     self.memo_no_first[1] = max(self.memo_no_first[0], self.memo_no_first[1])
-                elif h > 1:
+                else:
                     self.memo_no_first[h] = max(self.memo_no_first[h-1], self.memo_no_first[h] + self.memo_no_first[h-2])
             
         else:
-            for h, (key, val) in enumerate(self.memo_no_last.items()):
+            for h in range(1, idx+1):
                 if h == 1:
                     self.memo_no_last[1] = max(self.memo_no_last[0], self.memo_no_last[1])
-                elif h > 1:
+                else:
                     self.memo_no_last[h] = max(self.memo_no_last[h-1], self.memo_no_last[h] + self.memo_no_last[h-2])
 
         return self.memo_no_first[idx] if no_first else self.memo_no_last[idx]
