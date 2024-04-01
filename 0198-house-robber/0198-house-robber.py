8a616d7732b78e375b926edc8cc6b9787a51e9f1
Morgan Rockett <morgan.rockett@tufts.edu>
2024-04-01 19:31:01 -0400

    Time: 27 ms (96.02%), Space: 16.5 MB (89.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,21 +2,20 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         # store memoization where you can't rob negative money
         memo = OrderedDict((idx, max(0, val)) for idx, val in enumerate(nums))
-        i = len(nums)-1
 
-        return self._memo_rob(memo, i)
+        return self._memo_rob(memo, len(nums)-1)
 
     
     def _memo_rob(self, memo: OrderedDict, idx: int) -> int:
         if idx < 0:    return 0
         elif idx == 0: return memo[0]
-        
+            
         for h in range(1, idx+1):
             if h == 1:
                 memo[h] = max(memo[h-1], memo[h])
-            else:
-                memo[h] = max(memo[h-1], memo[h] + memo[h-2])
-        
-        print(f'{memo = }')
+                continue
+            memo[h] = max(memo[h-1], memo[h] + memo[h-2])
+
+        # print(f'{memo = }')
         return memo[idx]
 
