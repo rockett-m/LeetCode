f5096116386eb2952d994a845cc0073d532bfd7a
Morgan Rockett <morgan.rockett@tufts.edu>
2024-04-01 19:27:01 -0400

    Time: 38 ms (44.80%), Space: 16.5 MB (44.27%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,28 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        self.nums = nums
-        self.index = len(self.nums)-1
         # store memoization where you can't rob negative money
-        self.memo = OrderedDict((idx, max(0, val)) for idx, val in enumerate(self.nums))
-        
-        return self.recurse(self.index)
+        memo = OrderedDict((idx, max(0, val)) for idx, val in enumerate(nums))
+        i = len(nums)-1
+
+        return self._memo_rob(memo, i)
 
     
-    def recurse(self, idx: int) -> int:
-        if idx < 0:
-            return 0
-        elif idx == 0:
-            return self.nums[0]
+    def _memo_rob(self, memo: OrderedDict, idx: int) -> int:
+        if idx < 0:    return 0
+        elif idx == 0: return memo[0]
         
-        for h in range(idx+1):
-            if h == 0:
-                self.memo[0] = self.nums[0]
-            elif h == 1:
-                self.memo[1] = max(self.nums[0], self.nums[1])
+        for h in range(1, idx+1):
+            if h == 1:
+                memo[h] = max(memo[h-1], memo[h])
             else:
-                # self.nums[h] cannot be less than 0 (set above)
-                self.memo[h] = max(self.memo[h-1], self.nums[h] + self.memo[h-2])
-
-        # print(f'{self.memo = }')
-        return self.memo[idx]
+                memo[h] = max(memo[h-1], memo[h] + memo[h-2])
+        
+        print(f'{memo = }')
+        return memo[idx]
 
