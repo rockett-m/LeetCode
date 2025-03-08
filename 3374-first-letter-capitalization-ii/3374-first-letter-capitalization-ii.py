1f8a9174d84ae1c86f202c22d6c447bbf38183d3
Morgan Rockett <morgan.rockett@tufts.edu>
2025-03-08 09:26:13 -0500

    Time: 390 ms (66.43%), Space: 65.4 MB (87.77%) - LeetHub

--- 3374-first-letter-capitalization-ii/3374-first-letter-capitalization-ii.py
+++ 3374-first-letter-capitalization-ii/3374-first-letter-capitalization-ii.py
@@ -5,13 +5,10 @@ def capitalize_content(user_content: pd.DataFrame) -> pd.DataFrame:
     user_content['converted_text'] = None
     sents = user_content['content_text']
     for i in range(len(sents)):
-        words = sents[i].split(' ')
-        print(i, sents[i], words)
         out = ''
-        for w in words:
+        for w in sents[i].split(' '):
             if '-' in w:
-                z = w.split('-')
-                out += '-'.join([x.capitalize() for x in z])
+                out += '-'.join([x.capitalize() for x in w.split('-')])
             else:
                 out += w.capitalize()
             out += ' '
