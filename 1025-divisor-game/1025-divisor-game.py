e56f08cb071af282019f5efee715d1461af08baa
Morgan Rockett <morgan.rockett@tufts.edu>
2025-05-03 17:38:52 -0400

    Time: 0 ms (100%), Space: 17.9 MB (40.98%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,13 +1,9 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
         turns = 0
-        while n > 1:
-            x = 1
 
-            if n % x == 0:
-                turns += 1
-                n -= x
-            else:
-                break
+        while n > 1:
+            turns += 1
+            n -= 1
 
         return True if turns % 2 == 1 else False
