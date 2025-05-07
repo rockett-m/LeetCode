e63de129b5bb156cb66e47ec2ff58ba7dc196619
Morgan Rockett <morgan.rockett@tufts.edu>
2025-05-06 23:40:18 -0400

    Time: 115 ms (5.72%), Space: 18.2 MB (53.05%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def doesAliceWin(self, s: str) -> bool:
+        count = 0
+        vowels = ['a', 'e', 'i', 'o', 'u']
+
+        for idx, val in enumerate(s):
+            if val in vowels:
+                count += 1
+
+        # Alice can't move
+        if count == 0: return False
+        # leaves Bob no letters left
+        if count % 2 == 1: return True
+
+        return True
+        # 2+ even count of vowels
+        # let Alice delete all vowels and cons after up until final vowel
+        # forces Bob to delete 1 vowel which he isn't allowed to do - loses
+        
