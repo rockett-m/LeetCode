d942e96632aae52171052c9b6a5e022d0c413911
Morgan Rockett <morgan.rockett@tufts.edu>
2025-05-06 23:44:46 -0400

    Time: 123 ms (5.72%), Space: 18.1 MB (53.05%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.py
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.py
@@ -7,12 +7,14 @@ class Solution:
             if val in vowels:
                 count += 1
 
-        # Alice can't move
-        if count == 0: return False
-        # leaves Bob no letters left
-        if count % 2 == 1: return True
+        return False if count == 0 else True
 
-        return True
+        # Alice can't move
+        # if count == 0: return False
+        # leaves Bob no letters left - delete entire string
+        # if count % 2 == 1: return True
+
+        # return True
         # 2+ even count of vowels
         # let Alice delete all vowels and cons after up until final vowel
         # forces Bob to delete 1 vowel which he isn't allowed to do - loses
